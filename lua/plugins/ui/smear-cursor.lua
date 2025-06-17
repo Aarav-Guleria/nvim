31d125fb98c1a0cd8475ac897673dbb410e4be42
Aarav-Guleria <alatus1820@gmail.com>
2025-06-18 01:27:54 +0530

    added plugins, configured some plugins

--- lua/plugins/ui/smear-cursor.lua
+++ lua/plugins/ui/smear-cursor.lua
@@ -8,23 +8,4 @@ return {
     smear_cmdline_normal_mode = false,
     smear_cmdline_insert_mode = false,
   },
-  config = function(_, opts)
-    require("smear").setup(opts)
-
-    --Disable smearcursor on Snacks cmdline open
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "SnacksCmdlineEnter",
-      callback = function()
-        vim.g.smear_cursor_enabled = false
-      end,
-    })
-
-    --Reenable smearcursor when Snacks cmdline closes
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "SnacksCmdlineLeave",
-      callback = function()
-        vim.g.smear_cursor_enabled = true
-      end,
-    })
-  end,
 }
